3bc66b1474e56b9d65e534983c31f5216992b69a
66tuna <66tuna1018@gmail.com>
2022-05-31 21:26:52 +0900

    s0531

---
+++ js/board.js
@@ -0,0 +1,38 @@
+var boardList = [
+    {
+        pName: '페이지 네이션', // 프로젝트 이름
+        name: '임나나', // 작성자
+        date: '8월 4일'
+    },
+    {
+        pName: '페이지 네이션2', // 프로젝트 이름
+        name: '임누누', // 작성자
+        date: '8월 4일 1초'
+    },
+    {
+        pName: '페이지 네이션3', // 프로젝트 이름
+        name: '임노노', // 작성자
+        date: '8월 4일 2초'
+    },
+    {
+        pName: '페이지 네이션4', // 프로젝트 이름
+        name: '임부각', // 작성자
+        date: '8월 4일 3초'
+    },
+    {
+        pName: '페이지 네이션5', // 프로젝트 이름
+        name: '임도도', // 작성자
+        date: '8월 4일 4초'
+    },
+    {
+        pName: '페이지 네이션6', // 프로젝트 이름
+        name: '임수수', // 작성자
+        date: '8월 4일 5초'
+    }
+];
+
+function paging(totalData, currentPage){
+    const pageCount = 5; // 한 화면에 보여줄 페이지 갯수
+    var nowPage = 1;
+    
+}
